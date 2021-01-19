4bab2253b991be932c8fd1bcd127998d78d4f419
beppo <beppo@localhost.localdomain>
2021-01-19 12:55:22 -0600

    add: API key

--- lib/teacher_web/controllers/user_controller.ex
+++ lib/teacher_web/controllers/user_controller.ex
@@ -4,7 +4,9 @@ defmodule TeacherWeb.UserController do
   alias Teacher.Auth
 
   def show(conn, %{"id" => id}) do
-    user = Auth.get_user!(id)
+    user = id
+      |> Auth.get_user!()
+      |> Auth.preload_api_key()
     render(conn, "show.html", user: user)
   end
 
