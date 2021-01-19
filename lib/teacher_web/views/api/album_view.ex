3b49f163788b16b6a8026f43e46a56f5357aa8a9
beppo <beppo@localhost.localdomain>
2021-01-19 10:53:59 -0600

    [api] album  controller & view

--- lib/teacher_web/views/api/album_view.ex
+++ lib/teacher_web/views/api/album_view.ex
@@ -1,6 +1,20 @@
 defmodule TeacherWeb.Api.AlbumView do
   use TeacherWeb, :view
 
+  def render("index.json", %{albums: albums}) do
+    %{data: render_many(albums, TeacherWeb.Api.AlbumView, "album.json")}
+  end
 
+  def render("show.json", %{album: album}) do
+    %{data: render_one(album, TeacherWeb.Api.AlbumView, "Album.json")}
+  end
+
+  def render("album.json", %{album: album}) do
+    %{
+      id: album.id,
+      artist: album.artist,
+      title: album.title
+    }
+  end
 
 end
